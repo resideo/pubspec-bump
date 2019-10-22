8fd7f45ac647971b44703210ce3e56b1b00a386e
Nico Spencer <nicholas.spencer@resideo.com>
2019-10-21 21:33:07 -0500

    Bump from git

--- bump.sh
+++ bump.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+
+# config
+default_semvar_bump=${DEFAULT_BUMP:-minor}
 
 # get latest tag
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
@@ -8,22 +10,32 @@ tag_commit=$(git rev-list -n 1 $tag)
 # get current commit hash for tag
 commit=$(git rev-parse HEAD)
 
-echo $tag
-echo $tag_commit
-echo $commit
-
 if [ "$tag_commit" == "$commit" ]; then
     echo "No new commits since previous tag. Skipping..."
-    # exit 0
+    exit 0
 fi
 
-# if there are none, abort
+# if there are none, start tags at 0.0.0
 if [ -z "$tag" ]
 then
-    echo "No tag to sync pubspec with"
+    log=$(git log --pretty=oneline)
+    tag=0.0.0
+else
+    log=$(git log $tag..HEAD --pretty=oneline)
 fi
 
-yq w -i pubspec.yaml version $tag
+# get commit logs and determine home to bump the version
+# supports #major, #minor, #patch (anything else will be 'minor')
+case "$log" in
+    *#major* ) new=$(semver bump major $tag);;
+    *#minor* ) new=$(semver bump minor $tag);;
+    *#patch* ) new=$(semver bump patch $tag);;
+    * ) new=$(semver bump `echo $default_semvar_bump` $tag);;
+esac
+
+echo $new
+
+yq w -i pubspec.yaml version $new
 
 git config --local user.email "resideo.machine@resideo.com"
 git config --local user.name "Pubspec Bump"
